b230b8c108fd7bcdda7ec82d2434be74bd653c7b
MoniqueMS <monique.msilva06@gmail.com>
2026-05-14 10:24:54 -0300

    Update main.py

--- Sistema simples de cadastramento/main.py
+++ Sistema simples de cadastramento/main.py
@@ -1,68 +1,54 @@
 print("\n--------------------------------------------------")
 print("- Sistema simples para cadastramento de usuários -")
-print("--------------------------------------------------")
+print("--------------------------------------------------\n")
 
 usuarios = []
 
-while True:
+def cadastrar():
+    print("\n-------------")
+    print("- Cadastrar -")
+    print("-------------\n")
+    nome = input("Nome: ")
+    idade = input("Idade: ")
+
+    usuarios.append({
+        "nome": nome,
+        "idade": idade
+    })
+
+    print("\nUsuário cadastrado com sucesso!")
+
+#------------------------------------------------------------
+
+def listar():
     print("\n-------------------")
-    print("- Lista de opções -")
-    print("-------------------")
-    print("\n1. Cadastrar")
-    print("2. Listar")
-    print("3. Excluir")
-    print("4. Sair")
-
-    opcao = input("\nEscolha: ")
-
-    if opcao == "1":
-        print("\n------------")
-        print("- Cadastro -")
-        print("------------")
-        nome = input("\nNome: ")
-        idade = input("Idade: ")
-
-        usuario = {
-            "nome": nome,
-            "idade": idade
-        }
-
-        usuarios.append(usuario)
-        print("Usuário cadastrado.")
-
-    elif opcao == "2":
-        print("\n---------------------")
-        print("- Lista de cadastro -")
-        print("---------------------\n")
-        for i, u in enumerate(usuarios):
-            print(f"{i} - Nome: {u['nome']} | Idade: {u['idade']}")
-
-        input("\nPressione ENTER para voltar ao menu...")
-
-    elif opcao == "3":
-        print("\n-------------------")
-        print("- Excluir usuário -")
-        print("-------------------\n")
-
-        if len(usuario) == 0:
-            print("Nenhum usuário foi cadastrado ainda.")
-        else:
-            for i, u in enumerate(usuarios):
-                print(f"{i} - {u['nome']} ({u['idade']})")
-
-            try:
-                indice = int(input("\nDigite o número do usuário: "))
-
-                if 0 <= indice < len(usuario):
-                    removido = usuarios.pop(indice)
-                    print(f"\nUsuário {removido['nome']} removido!")
-                else:
-                    print("índice inválido.")
-            except ValueError:
-                print("Digite um número válido.")
-
-    elif opcao == "4":
-        break
-
+    print("- Listar usuários -")
+    print("-------------------\n")
+    if len(usuarios = 0):
+        print("Nenhum usuário foi cadastrado ainda.")
+        input("\nPressione ENTER para continuar...")
     else:
-        print("\nOpção inválida, tente novamente.")
+        for i, u in enumerate(usuarios):
+            print(f"Nome: {u['nome]'} | Idade: {u['idade']}")
+            input("\nPressione ENTER para continuar...")
+#------------------------------------------------------------
+
+def excluir()
+    print("\n-------------------")
+    print("- Excluir usuário -")
+    print("-------------------\n")
+
+    if len(usuarios = 0):
+        print("Nenhum usuário foi cadastrado ainda.")
+        input("\nPressione ENTER para continuar...")
+    else:
+        for i, u in enumarate(usuarios);
+        print(f"ID [{i}] - {u['nome']} ({u['idade']})")
+        
+    try:
+        indice = int(input("\nDigite o número do usuário que deseja excluir: "))
+        if 0 <= indice < len(usuarios):
+            removido = usuario.pop(indice)
+            print(f"Usuário {removido['nome']} removido.")
+        else:
+            print("Número inválido.")
